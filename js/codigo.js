1f1b6e01c7c04f8b359d64bfc49b33ce8276e43b
maxi-guerra <124089489+maxi-guerra@users.noreply.github.com>
2023-02-01 15:06:19 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById('boton_fb').addEventListener('click', function(){
+    console.log();
+    document.getElementById("demo").innerHTML = "Alfred Fulanito deTal"
+});
+
+document.getElementById('boton_ig').addEventListener('click', function(){
+    console.log();
+    document.getElementById("demo").innerHTML = "Alfred deTal.23"
+});
+
+document.getElementById('boton_tw').addEventListener('click', function(){
+    console.log();
+    document.getElementById("demo").innerHTML = "Fulanito_deTal_1805"
+});
